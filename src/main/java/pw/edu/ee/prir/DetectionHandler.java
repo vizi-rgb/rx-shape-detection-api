0b8ef5723855b90eff41bfdaafc739df5c46937c
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2024-06-07 15:17:10 +0200

    Changed to Handler and Router Functions

---
+++ src/main/java/pw/edu/ee/prir/DetectionHandler.java
@@ -0,0 +1,18 @@
+package pw.edu.ee.prir;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class DetectionHandler {
+
+    public Mono<ServerResponse> uploadFile(ServerRequest request) {
+        return ServerResponse.ok().body(
+            BodyInserters.fromValue("File uploaded")
+        );
+    }
+
+}
---
+++ src/main/java/pw/edu/ee/prir/DetectionRouter.java
@@ -0,0 +1,29 @@
+package pw.edu.ee.prir;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
+@Configuration(proxyBeanMethods = false)
+public class DetectionRouter {
+
+    private static final String DETECTION_PATH = "/detection";
+
+    @Bean
+    public RouterFunction<ServerResponse> route(DetectionHandler detectionHandler) {
+
+        return RouterFunctions
+            .route(
+                POST("%s/upload".formatted(DETECTION_PATH))
+                    .and(accept(MediaType.MULTIPART_FORM_DATA)),
+                detectionHandler::uploadFile
+            );
+    }
+
+}
